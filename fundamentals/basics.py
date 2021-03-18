fbe0f4b726a53a50454918b498b9b8588847d2a4
Ramkumar Krishnan <ramkumarkrishnancareer@gmail.com>
2021-03-17 21:19:18 -0700

    Part 1: Basics,Function,Loops

---
+++ fundamentals/basics.py
@@ -0,0 +1,196 @@
+# The 1000th 'first program' in Python - let us hope it is the last first
+"""Alternative commenting in Python"""
+print ("Hello World!")
+print ("Python 3", end="")
+print ("Coding")
+
+# Basic datatypes
+print ("\nDish out some integers!")
+print (10)
+print (-3000)
+num = 4000
+print (num)
+num = -1600
+print (num)
+
+print ("Fillet some floating points")
+print (1.0005)
+print (-85.601)
+fltpt = 1.234
+print (fltpt)
+
+print ("\nCollate some complex numbers")
+print ("Complex(real, imaginary) (a + bj)")
+print (complex(10, 20))
+cplx = complex(2, 0)
+cplx2 = complex(0, 2)
+# Python does not like this string
+# print ("Cplx: " + cplx + "  Cplx2: " + cplx2)
+# Python does not mind the semicolons - perhaps worth using it
+print (cplx);
+print (cplx2)
+
+print ("\nBelt out Booleans!")
+print (True)
+maqbool = False
+print (maqbool)
+# the following won't work - first letter needs to be capital
+# maqbool = false
+
+print ("\nSwing some Strings")
+lp = 'Learning Python in single quotes'
+print (lp)
+print ("&")
+empty = ""
+print (empty)
+print (len(lp))
+lp = "Learning Python in double quotes"
+print (lp)
+print (len(lp))
+print (lp[9])
+print (lp[len(lp) - 1])
+# the following will error
+# print (lp[len(lp)])
+print ("Reverse indexing using [-idx], get P in Python")
+print (lp[-1])  # same as len[len(lp)-1]
+print (lp[-23]) # same as lp[9]
+
+print ("\nStep 'em Strings")
+print (lp[9:15:2])
+print (lp[9::2])
+
+print ("\nReverse 'em Strings - unique to Python")
+print (lp[::-1])
+print (lp[15:2:-1])
+print (lp[18:0:-2])
+
+print ("\nSlice 'em Strings - unique to Python")
+print (lp[9:15])
+print (lp[8:len(lp)])
+print (lp[:9])
+print (lp[9:])
+
+print ("\nPEMDAS arithmetic Operators")
+print (10 + 5)
+print (10 - 5)
+print (10 * 5)
+print (10 / 5)
+print (11 // 5) # floor division - floored to nearest smaller integer
+print (14.9999 // 5)
+print (10 % 5)
+print (-10 % 5)
+print (10 % -5) # remainder is -ve is rhs operand is -ve
+print (14 % 5)
+print (14.9999 % 5)
+
+print ("\nComparison operators")
+print (10 < 5)
+print (5 < 10)
+print (5 == 5)
+print (5 <= 10)
+print (5 != 10)
+print (5 is not 10)
+print (10/2 is 5)
+print (10/2 is not 6)
+
+print ("\nLogical operators")
+print ((10 < 5) and (5 < 10))
+print ((10 < 5) or (5 < 10))
+print (not (10 < 5))
+
+print ("\nBitwise operators")
+n1 = 10 #0000 1010
+n2 = 20 #0001 0100
+print (n1 & n2) #0000 0000
+print (n1 | n2) #0001 1110
+print (n1 ^ n2) #0001 1110
+print (~n1)     #1111 0101
+print (n1 << 3) #0101 0000
+print (n2 >> 3) #0000 0101
+
+print ("\nString comparison operators")
+s1 = 'Learning'
+s2 = 'Python'
+print (s1 < s2) # string first in the dictionary has smaller value
+s3 = s2
+print (s2 == s3) 
+print (s3 < s1)
+print (s1 >= s3)
+
+print ("\nString concat and find - Important")
+print (s1 + " " + s2)
+print (s2 * 3)
+print ('Py' in s2)
+print ('Learn' in s1)
+
+print ("\nGrouping operator - unique to Python")
+print (s1, s2, n1, n2, 10*5)
+mylist = [s1, s2, n1, n2, 10*5]
+print (mylist)
+print (mylist[3])
+print (len(mylist))
+
+print ("\nMixed type operations - unique to Python")
+print (True + 5) # True gets a value 1, False 0
+x = 20; y = 5;
+print ((x + True) / (4 - y * False))
+
+print ("\nQuiz")
+mystr = "0123456789"
+print (mystr[2:6:2])
+print (mystr[-2])
+print (mystr[-4])
+print (mystr[-2:-8])
+print (mystr[-2:-8:-2])
+
+print ("Educative IO Exercise Gravitational Force between earth and moon")
+print ("Compute GMm/r^2 with inputs G=6.67*10^-11, M=6.0*10^24, m=7.34*10^22, r=3.84*10^8")
+G = 6.67 * (10 ** -11)
+M = 6.0 * (10 ** 24)
+m = 7.34 * (10 ** 22)
+r = 3.84 * (10 ** 8)
+
+Fg = (G * M * m)/(r ** 2)
+print (Fg)
+
+print ("\nEmpty (pass) statement")
+if (G < 1):
+    pass
+    print ("Enter more code above")
+    
+print ("\nConditional Statements")
+if (G < 1):
+    newG = G
+    print ("G is less than 1")
+    print (newG)
+else:
+    print ("G is more than 1")
+# Scoping sucks in Python
+print (newG)
+
+out = "G is less than 1" if (G < 1) else "G is more than 1"
+print (out)
+
+if (G < 1):
+    print ("G is less than 1")
+elif (G > 1):
+    print ("G is more than 1")
+else:
+    print ("G is 1")
+
+print ("\nEducative IO Exercise Discounted Price")
+price = 200
+final_price = price
+if (price >= 300):
+    final_price = price * ((100 - 30)/100) # 30% discount
+elif (price >= 200):
+    final_price = price * ((100 - 20)/100) # 20% discount
+elif (price >= 100):
+    final_price = price * ((100 - 10)/100) # 10% discount
+elif (price > 0):
+    final_price = price * ((100 - 5)/100)  # 5% discount
+else:
+    print ("Error in price")
+print ("Price: " + str(price) + " Final price: " + str(final_price))
+
+
---
+++ fundamentals/loops.py
@@ -0,0 +1,175 @@
+# Python Loops
+# for [iterator] in [sequence]
+# [sequence] = range(start, end, step)
+
+print ("\nLoop with range")
+evenodd = lambda n : "even" if n % 2 == 0 else "odd"
+for i in range(1, 11):
+    print (i, "is " + str(evenodd(i)))
+
+print ("\nLoop in range with step")
+for i in range(1, 11, 2):
+    print (i, "is " + str(evenodd(i)))
+
+print ("\nLoop through list")
+flist = [0.1, 1.1, 2.1, 3.1, 4.1, 5.1]
+for i in flist:
+    print (i)
+    
+print ("\nLoop through range cast on a list")
+for i in range(0, len(flist)):
+    print (flist[i])
+
+print ("\nLoop through range cast on a list")
+for i in range(0, len(flist), 2):
+    print (flist[i])
+
+print ("\nNested Loop for matrix multiplication")
+m1 = [1, 2, 3, 4] # single row matrix
+m2 = [4, 3, 2, 1] # single column matrix
+k = 0
+if len(m1) != len(m2):
+    print ("Incompatible matrix dimensions")
+for i in range(0, len(m1)):
+    for j in range(0, len(m2)):
+        if i == j:
+            k += m1[i] * m2[j]
+            print (str(i+1) + "," + str(j+1) + ": " + str(m1[i]*m2[j]))
+print ("Output: ", k)        
+
+#flist = ['m','a','l','a','y','a','l','a','m']
+
+print ("\nBrake for moose")
+for i in m1:
+    for j in m2:
+        print (str(i) + "," + str(j))
+        if i == j+1:
+            print (str(i) + " is one greater than " + str(j) + ". Exiting")
+            break # this only breaks out of the inner loop
+
+print ("\nContinue for coons")
+for i in m1:
+    for j in m2:
+        if i == j:
+            continue
+        print (str(i) + "," + str(j))
+
+print ("\nIt's been a while ...")
+flist = [1.0, 1.1, 1.2, 1.3, 1.4, 1.5]
+i = 1;
+while i in flist:
+    print ("i: " + str(i))
+    i += 0.2
+
+print ("\Quiz")
+strlist = ["Anakin", "Vader", "Luke", "Leia", "Rey"]
+for s in strlist:
+    if len(s) < 5:
+        print (s + ":" + str(len(s)))
+
+i = 0
+while (i < len(strlist)):
+    if len(strlist[i]) < 5:
+        print (len(strlist[i]))
+    i += 1
+
+print ("\nBalanced Braces")
+# Given "[[[[][][]]]" return true if left braces match with closing right braces
+#braces = "[[[[][][]]]"
+#braces = "[[[[]]]][]["
+#braces = "[[[[]]]]"
+#braces = "]]]][[[["
+braces = "[]][[]"
+left = 0
+right = 0
+i = 0
+brace_open = 0
+while i < len(braces):
+    # base case
+    if i == 0 and braces[i] == "]":
+        print ("False: starts with ]")
+        right += 1
+        break
+    if (braces[i] == "["):
+        left += 1
+        brace_open += 1
+    if (braces[i] == "]"):
+        right += 1
+        if brace_open >= 1:
+            brace_open -= 1
+    else:
+        pass # don't care
+    i += 1
+if left != right or brace_open > 0:
+    print ("False: Left:" + str(left) + " Right:" + str(right))
+else:
+    print ("True: Left:" + str(left) + " Right:" + str(right))
+
+print ("\nSimpler code")
+def check_balance(braces):
+    check = 0
+    for brace in braces:
+        if brace == '[':
+            check += 1
+        elif brace == ']':
+            check -= 1
+        if check < 0:
+            break
+    return check == 0
+
+#braces = "[[[[][][]]]"
+#braces = "[[[[]]]][]["
+#braces = "[[[[]]]]"
+#braces = "]]]][[[["
+#braces = "[]][[]"
+ret = check_balance(braces)
+if ret == True:
+    print (braces + " is balanced")
+else:
+    print (braces + " is unbalanced")
+
+print ("\nCheckSum to zero")
+def check_sum(nlist):
+    for num in nlist:
+        for complement in nlist:
+            if num + complement == 0:
+                return True
+    return False
+
+numlist = [10, -14, 26, 5, -3, 13, -5]
+#numlist = [10, -14, 26, 5, -3]
+if check_sum(numlist):
+    print ("Sum checks")
+else:
+    print ("Sum does not check")
+
+# 0 1 1 2 3 5 8 13 21
+print ("\nFibonacci with loops")
+def fib(n):
+    n1 = 0
+    n2 = 1
+    if (n < 1):
+        return -1
+    if (n == 1):
+        return n1
+    if (n == 2):
+        return n2
+    nth = 3
+    # if you use for loop here, Py complains that val is uninitialized
+    while (nth <= n):
+        val = n1 + n2
+        n1 = n2
+        n2 = val
+        nth += 1
+    return val
+
+print ("-1: " + str(fib(-1)))
+print ("0: " + str(fib(0)))
+print ("1: " + str(fib(1)))
+print ("2: " + str(fib(2)))
+print ("3: " + str(fib(3)))
+print ("4: " + str(fib(4)))
+print ("5: " + str(fib(5)))
+print ("6: " + str(fib(6)))
+print ("7: " + str(fib(7)))
+            
